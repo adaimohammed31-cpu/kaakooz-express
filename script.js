43a1e25beeab451a5ce0fbf137b249dc3e7d9d74
adaimohammed31-cpu <adaimohammed31@gmail.com>
2026-07-21 17:03:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
-// انتقال سلس
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
-anchor.addEventListener("click",function(e){
-
-e.preventDefault();
-
-const target=document.querySelector(this.getAttribute("href"));
-
-if(target){
-
-target.scrollIntoView({
-
-behavior:"smooth"
-
-});
-
-}
-
-});
-
-});
 
 // إظهار العناصر عند النزول
 const cards=document.querySelectorAll(".welcome-card,.menu-card,.location-card,.contact-card");
