4bc129682426a3a9624e757e18d0539b1a8b9df4
adaimohammed31-cpu <adaimohammed31@gmail.com>
2026-07-21 12:20:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 
+// كعكوز إكسبرس
+
+function showMenu() {
+
+const menu = document.getElementById("menu");
+
+if (!menu) return;
+
+if (menu.style.display === "block") {
+    menu.style.display = "none";
+} else {
+    menu.style.display = "block";
+}
+
+}
+
+window.addEventListener("scroll", function () {
+
+const nav = document.querySelector("nav");
+
+if (!nav) return;
+
+if (window.scrollY > 80) {
+
+nav.style.boxShadow = "0 8px 20px rgba(0,0,0,.35)";
+
+} else {
+
+nav.style.boxShadow = "0 5px 15px rgba(0,0,0,.25)";
+
+}
+
+});
