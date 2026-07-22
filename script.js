97653b67a87c43f11ecfdfc22e569e780c7a7d18
adaimohammed31-cpu <adaimohammed31@gmail.com>
2026-07-23 02:01:16 +0300

    Add lightbox functionality for image gallery

--- script.js
+++ script.js
@@ -82,3 +82,24 @@ galleryBtn.addEventListener("click", function(e) {
         behavior: "smooth"
     });
 });
+// نافذة تكبير الصور
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightboxImg");
+const closeBtn = document.querySelector(".close");
+
+document.querySelectorAll(".gallery img").forEach(img => {
+    img.addEventListener("click", function () {
+        lightbox.style.display = "flex";
+        lightboxImg.src = this.src;
+    });
+});
+
+closeBtn.addEventListener("click", function () {
+    lightbox.style.display = "none";
+});
+
+lightbox.addEventListener("click", function (e) {
+    if (e.target === lightbox) {
+        lightbox.style.display = "none";
+    }
+});
