8d1aed508a63912caf2bea9b5dca19e2ecedfb85
adaimohammed31-cpu <adaimohammed31@gmail.com>
2026-07-25 00:38:20 +0300

    Initialize Firebase app and Firestore database

--- firebase.js
+++ firebase.js
@@ -1,1 +1,19 @@
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAClyqUeUhWBBOHhhriVW04HZxzP3tgOOs",
+  authDomain: "kakooz-express.firebaseapp.com",
+  projectId: "kakooz-express",
+  storageBucket: "kakooz-express.firebasestorage.app",
+  messagingSenderId: "755647745722",
+  appId: "1:755647745722:web:3476aac170791be0b4bc83",
+  measurementId: "G-KNQFRXV7PB"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
