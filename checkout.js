d7332808777c8de5a7a45e55e7e7d440cffaff85
adaimohammed31-cpu <adaimohammed31@gmail.com>
2026-07-26 22:13:06 +0300

    Remove automatic location fetching in checkout.js
    Removed automatic location fetching on map initialization.

--- checkout.js
+++ checkout.js
@@ -142,8 +142,7 @@ function updateDeliveryFields() {
 
                 initMap();
 
-                // تحديد الموقع تلقائياً
-                getCurrentLocation();
+            
 
             } else {
 
